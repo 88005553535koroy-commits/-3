b91af0a6a549189a2d946d56f316f312a34d2fa3
Koroy Lera <88005553535koroy@gmail.com>
2026-02-25 19:41:44 +0300

    Add files via upload

---
+++ 3.2.py
@@ -0,0 +1,9 @@
+from AppData.Local.Programs.Python.Python313.Lib.test.test_selectors import SelectSelectorTestCase
+
+a = float (input("число а"))
+b = float (input("число b"))
+if a!=0:
+    x=-b/a
+    print (f"решенгие урав: x = {x}")
+else:
+    print("нет решениЯ")
